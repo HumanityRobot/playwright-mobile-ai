39862ddfbbe454865fd54cb188ada36cac8e0309
sahril.rahmatulloh <sahril.rahmatulloh@bcadigital.co.id>
2026-08-27 01:03:00 +0700

    Ketinggalan

--- tests/mobile/BLU-Experience/PusatBantuan.spec.ts
+++ tests/mobile/BLU-Experience/PusatBantuan.spec.ts
@@ -1,3 +1,13 @@
 import { test } from '../../../src/mobile/mobile.fixture';
 
-m
+test(
+  'Launch blu application - click Yuk Mulai',
+  {
+    tag: ['@smoke', '@launch'],
+  },
+  async ({ mobile }) => {
+    await mobile.click('login_btnYukMulai');
+
+    await mobile.screenshot('LaunchApp_YukMulai');
+  }
+);
